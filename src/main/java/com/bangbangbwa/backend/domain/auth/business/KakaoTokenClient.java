333a352e44af350c79069e787c06cb853c595713
JinTaekLim <jintaek019@gmail.com>
2024-10-19 22:48:57 +0900

    [BBB-17] KakaoTokenClient @PathVariable -> @RequestParm 변경

--- src/main/java/com/bangbangbwa/backend/domain/auth/business/KakaoTokenClient.java
+++ src/main/java/com/bangbangbwa/backend/domain/auth/business/KakaoTokenClient.java
@@ -3,22 +3,16 @@ package com.bangbangbwa.backend.domain.auth.business;
 
 import com.bangbangbwa.backend.domain.auth.common.dto.OAuthTokenDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "KakaoOAuth", url = "https://kauth.kakao.com")
 public interface KakaoTokenClient {
-  @PostMapping(
-      value = "/oauth/token?" +
-          "code={CODE}" +
-          "&client_id={CLIENT_ID}" +
-          "&client_secret={CLIENT_SECRET}" +
-          "&redirect_uri={REDIRECT_URI}" +
-          "&grant_type={GRANT_TYPE}")
+  @PostMapping(value = "/oauth/token")
   OAuthTokenDto getAccessToken(
-      @PathVariable("CODE") String code,
-      @PathVariable("CLIENT_ID") String clientId,
-      @PathVariable("CLIENT_SECRET") String clientSecret,
-      @PathVariable("REDIRECT_URI") String redirectUri,
-      @PathVariable("GRANT_TYPE") String grantType);
+      @RequestParam("code") String code,
+      @RequestParam("client_id") String clientId,
+      @RequestParam("client_secret") String clientSecret,
+      @RequestParam("redirect_uri") String redirectUri,
+      @RequestParam("grant_type") String grantType);
 }
