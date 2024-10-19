0ab6672ebcdb929ba528b688fa14449a0f3521a8
JinTaekLim <jintaek019@gmail.com>
2024-10-19 22:57:23 +0900

    [BBB-17] KakaoUserClient 하드코딩 값 상수 변경

--- src/main/java/com/bangbangbwa/backend/domain/auth/business/KakaoUserClient.java
+++ src/main/java/com/bangbangbwa/backend/domain/auth/business/KakaoUserClient.java
@@ -2,19 +2,22 @@ package com.bangbangbwa.backend.domain.auth.business;
 
 import com.bangbangbwa.backend.domain.auth.common.dto.KakaoInfoDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(
-    name = "KakaoResource",
-    url = "https://kapi.kakao.com")
+@FeignClient(name = "KakaoResource", url = "https://kapi.kakao.com")
 public interface KakaoUserClient {
+
+  String CONTENT_TYPE_VALUE = "application/x-www-form-urlencoded;charset=utf-8";
+  String BEARER_PREFIX = "Bearer ";
+
   @GetMapping(value = "/v2/user/me")
   KakaoInfoDto getInfo(
-      @RequestHeader(value = "Authorization") String accessToken,
-      @RequestHeader(value = "Content-Type", defaultValue = "application/x-www-form-urlencoded;charset=utf-8") String contentType);
+      @RequestHeader(value = HttpHeaders.AUTHORIZATION) String accessToken,
+      @RequestHeader(value = HttpHeaders.CONTENT_TYPE, defaultValue = CONTENT_TYPE_VALUE) String contentType);
 
   default KakaoInfoDto getInfo(String accessToken) {
-    return getInfo("Bearer " + accessToken, "application/x-www-form-urlencoded;charset=utf-8");
+    return getInfo(BEARER_PREFIX + accessToken, CONTENT_TYPE_VALUE);
   }
 }
