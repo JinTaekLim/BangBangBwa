056ec31bacd998944f956eb74a1542532913afda
hyensukim <crazymin17@naver.com>
2024-10-20 23:48:15 +0900

    [BBB-11] config 패키지로 이전

--- src/main/java/com/bangbangbwa/backend/global/config/SecurityConfig.java
+++ src/main/java/com/bangbangbwa/backend/global/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.bangbangbwa.backend.global;
+package com.bangbangbwa.backend.global.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
