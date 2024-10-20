075cdb8ce19aa26d5c5dca18876877a70dffba0d
hyensukim <crazymin17@naver.com>
2024-10-21 00:04:02 +0900

    [BBB-11] Swagger 문서화

---
+++ src/main/java/com/bangbangbwa/backend/domain/BasicApi.java
@@ -0,0 +1,18 @@
+package com.bangbangbwa.backend.domain;
+
+import com.bangbangbwa.backend.global.annotation.ApiCommonResponse;
+import com.bangbangbwa.backend.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Basic (기본) API", description = "기본 제공 API")
+@ApiCommonResponse
+public interface BasicApi {
+
+  @Operation(summary = "서비스 정상 여부 확인", tags = {"Basic (기본) API"})
+  ApiResponse<?> healthCheck();
+
+  @Operation(summary = "예외 핸들링", tags = {"Basic (기본) API"})
+  ApiResponse<?> error(@Parameter String name);
+}
