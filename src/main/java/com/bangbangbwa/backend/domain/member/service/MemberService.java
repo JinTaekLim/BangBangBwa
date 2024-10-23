df64b626427c7d8eb92badfab8bd98e758838c9e
hyensukim <crazymin17@naver.com>
2024-10-23 21:35:58 +0900

    [BBB-14] Member update 비지니스 로직 클래스 정의

--- src/main/java/com/bangbangbwa/backend/domain/member/service/MemberService.java
+++ src/main/java/com/bangbangbwa/backend/domain/member/service/MemberService.java
@@ -1,11 +1,10 @@
 package com.bangbangbwa.backend.domain.member.service;
 
+import com.bangbangbwa.backend.domain.member.business.MemberUpdater;
 import com.bangbangbwa.backend.domain.member.common.Member;
 import com.bangbangbwa.backend.domain.oauth.common.OAuthInfoDto;
 import com.bangbangbwa.backend.domain.token.common.TokenDto;
 import com.bangbangbwa.backend.domain.token.service.TokenService;
-import com.bangbangbwa.backend.global.util.S3Manager;
-import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,15 +13,12 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 public class MemberService {
 
-  private final S3Manager s3Manager;
   private final TokenService tokenService;
+  private final MemberUpdater memberUpdater;
 
   public TokenDto signup(OAuthInfoDto oAuthInfo, Member member, MultipartFile profileFile) {
-    if (Objects.nonNull(profileFile)) {
-      String profile = s3Manager.upload(profileFile);
-      member.updateProfile(profile);
-    }
-    member.addOAuthInfo(oAuthInfo);
+    memberUpdater.updateProfile(member, profileFile);
+    memberUpdater.updateOAuthInfo(member, oAuthInfo);
     return tokenService.getToken(member);
   }
 }
