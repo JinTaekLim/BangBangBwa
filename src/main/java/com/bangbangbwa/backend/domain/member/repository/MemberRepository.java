c5b8f5edec58f822c8e9d315d35863fad47c82b3
JinTaekLim <jintaek019@gmail.com>
2024-10-27 18:43:10 +0900

    [BBB-33] MemberRepository findById 추가

--- src/main/java/com/bangbangbwa/backend/domain/member/repository/MemberRepository.java
+++ src/main/java/com/bangbangbwa/backend/domain/member/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package com.bangbangbwa.backend.domain.member.repository;
 
 import com.bangbangbwa.backend.domain.member.common.entity.Member;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
@@ -13,5 +14,9 @@ public class MemberRepository {
 
   public void save(Member member) {
     mysql.insert("MemberMapper.save", member);
+  }
+
+  public Optional<Member> findById(Long memberId) {
+    return Optional.ofNullable(mysql.selectOne("MemberMapper.findById", memberId));
   }
 }
