6cf511618ca4817c9fdc34c602a33db42a4c2408
JinTaekLim <jintaek019@gmail.com>
2024-11-01 23:08:21 +0900

    [BBB-17] 미사용 코드 삭제

--- src/main/java/com/bangbangbwa/backend/domain/auth/common/dto/OAuthTokenDto.java
+++
@@ -1,9 +0,0 @@
-package com.bangbangbwa.backend.domain.auth.common.dto;
-
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record OAuthTokenDto(
-    @JsonProperty("access_token")
-    String accessToken
-){ }
