cf1b9948b703b76334dbee1a133a9e6e55b1d5fa
JinTaekLim <jintaek019@gmail.com>
2024-11-04 14:17:50 +0900

    [BBB-51] SnsService createComment 추가

--- src/main/java/com/bangbangbwa/backend/domain/sns/service/SnsService.java
+++ src/main/java/com/bangbangbwa/backend/domain/sns/service/SnsService.java
@@ -1,12 +1,16 @@
 package com.bangbangbwa.backend.domain.sns.service;
 
 import com.bangbangbwa.backend.domain.member.business.MemberProvider;
-import com.bangbangbwa.backend.domain.member.business.MemberReader;
 import com.bangbangbwa.backend.domain.member.business.MemberValidator;
 import com.bangbangbwa.backend.domain.member.common.entity.Member;
+import com.bangbangbwa.backend.domain.sns.business.CommentCreator;
+import com.bangbangbwa.backend.domain.sns.business.CommentGenerator;
 import com.bangbangbwa.backend.domain.sns.business.PostCreator;
 import com.bangbangbwa.backend.domain.sns.business.PostGenerator;
+import com.bangbangbwa.backend.domain.sns.business.PostReader;
+import com.bangbangbwa.backend.domain.sns.common.dto.CreateCommentDto;
 import com.bangbangbwa.backend.domain.sns.common.dto.CreatePostDto;
+import com.bangbangbwa.backend.domain.sns.common.entity.Comment;
 import com.bangbangbwa.backend.domain.sns.common.entity.Post;
 import com.bangbangbwa.backend.domain.sns.common.enums.PostType;
 import lombok.RequiredArgsConstructor;
@@ -18,12 +22,13 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class SnsService {
 
-  private final MemberReader memberReader;
   private final MemberProvider memberProvider;
   private final MemberValidator memberValidator;
   private final PostGenerator postGenerator;
   private final PostCreator postCreator;
-
+  private final PostReader postReader;
+  private final CommentGenerator commentGenerator;
+  private final CommentCreator commentCreator;
 
   public Post createPost(CreatePostDto.Request request) {
     Member member = memberProvider.getCurrentMember();
@@ -34,5 +39,13 @@ public class SnsService {
     postCreator.save(post);
 
     return post;
+  }
+
+  public Comment createComment(CreateCommentDto.Request request) {
+    Member member = memberProvider.getCurrentMember();
+    postReader.findById(request.postId());
+    Comment comment = commentGenerator.generate(request, member);
+    commentCreator.save(comment);
+    return comment;
   }
 }
