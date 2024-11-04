04748a2667f277c585f54d9c024887850bbf9294
JinTaekLim <jintaek019@gmail.com>
2024-11-04 14:16:58 +0900

    [BBB-51] CommentGenerator 추가

---
+++ src/main/java/com/bangbangbwa/backend/domain/sns/business/CommentGenerator.java
@@ -0,0 +1,21 @@
+package com.bangbangbwa.backend.domain.sns.business;
+
+import com.bangbangbwa.backend.domain.member.common.entity.Member;
+import com.bangbangbwa.backend.domain.sns.common.dto.CreateCommentDto;
+import com.bangbangbwa.backend.domain.sns.common.entity.Comment;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CommentGenerator {
+
+  private final CommentParser commentParser;
+  private final CommentUpdater commentUpdater;
+
+  public Comment generate(CreateCommentDto.Request request, Member member) {
+    Comment comment = commentParser.requestToEntity(request);
+    commentUpdater.updateMemberId(comment, member);
+    return comment;
+  }
+}
