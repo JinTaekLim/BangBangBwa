7ea42832dff1430bde176903199d6bcb782cfaf5
JinTaekLim <jintaek019@gmail.com>
2024-11-05 20:11:33 +0900

    [BBB-58] Admin Entity 추가

---
+++ src/main/java/com/bangbangbwa/backend/domain/admin/common/entity/Admin.java
@@ -0,0 +1,16 @@
+package com.bangbangbwa.backend.domain.admin.common.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Admin {
+
+  private Long id;
+
+
+
+}
