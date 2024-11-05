721438ef769686ee15a8234e3fccc44bbc682fa7
JinTaekLim <jintaek019@gmail.com>
2024-11-05 14:14:12 +0900

    [BBB-57] DailyMessage entity 추가

---
+++ src/main/java/com/bangbangbwa/backend/domain/streamer/common/entity/DailyMessage.java
@@ -0,0 +1,23 @@
+package com.bangbangbwa.backend.domain.streamer.common.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DailyMessage {
+
+  private Long streamerId;
+  private String message;
+  private Long expirationTime;
+
+  @Builder
+  public DailyMessage(Long streamerId, String message) {
+    this.streamerId = streamerId;
+    this.message = message;
+    this.expirationTime = 24L;
+  }
+}
