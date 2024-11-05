0511c10d37da66a5423f378c0161a8c85b14620d
JinTaekLim <jintaek019@gmail.com>
2024-11-05 20:14:35 +0900

    [BBB-58] PendingStreamerUpdater 추가

---
+++ src/main/java/com/bangbangbwa/backend/domain/streamer/common/business/PendingStreamerUpdater.java
@@ -0,0 +1,18 @@
+package com.bangbangbwa.backend.domain.streamer.common.business;
+
+import com.bangbangbwa.backend.domain.streamer.common.entity.PendingStreamer;
+import com.bangbangbwa.backend.domain.streamer.repository.PendingStreamerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PendingStreamerUpdater {
+
+  private final PendingStreamerRepository pendingStreamerRepository;
+
+  public void updateAdminId(PendingStreamer pendingStreamer) {
+    pendingStreamerRepository.updateAdminId(pendingStreamer);
+  }
+
+}
