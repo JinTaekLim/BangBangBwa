ee1e55dec9dce39ae275b64d46ef55e00fe0894b
JinTaekLim <jintaek019@gmail.com>
2024-11-09 22:39:49 +0900

    [BBB-71] PostUpdater updateId, status 추가

--- src/main/java/com/bangbangbwa/backend/domain/sns/business/PostUpdater.java
+++ src/main/java/com/bangbangbwa/backend/domain/sns/business/PostUpdater.java
@@ -2,6 +2,7 @@ package com.bangbangbwa.backend.domain.sns.business;
 
 import com.bangbangbwa.backend.domain.member.common.entity.Member;
 import com.bangbangbwa.backend.domain.sns.common.entity.Post;
+import com.bangbangbwa.backend.domain.sns.common.enums.PostStatus;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,4 +11,8 @@ public class PostUpdater {
   public void updateMemberId(Post post, Member member) {
     post.updateMemberId(member.getId());
   }
+
+  public void updateId(Post post, Long postId) { post.updateId(postId);}
+
+  public void updateStatus(Post post, PostStatus status) { post.updatedStatus(status);}
 }
