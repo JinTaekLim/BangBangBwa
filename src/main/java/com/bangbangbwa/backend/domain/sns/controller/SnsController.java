243e9c63fb2bdc96ed1a246e00371462b411be8b
JinTaekLim <jintaek019@gmail.com>
2024-11-09 22:44:43 +0900

    [BBB-71] SnsController createPost request.postId 의 유무에 따라 호출되는 service 다르게 변경

--- src/main/java/com/bangbangbwa/backend/domain/sns/controller/SnsController.java
+++ src/main/java/com/bangbangbwa/backend/domain/sns/controller/SnsController.java
@@ -102,7 +102,9 @@ public class SnsController implements SnsApi{
   public ApiResponse<CreatePostDto.Response> createPost(
       @RequestBody @Valid CreatePostDto.Request request
   ) {
-    Post post = snsService.createPost(request);
+    Post post = request.postId() == null ?
+        snsService.createPost(request): snsService.updatePost(request);
+
     CreatePostDto.Response response = PostMapper.INSTANCE.dtoToCreatePostResponse(post);
     return ApiResponse.ok(response);
   }
