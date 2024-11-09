7e51ece3df679fb11bdb717e8e37acd06cbfd965
JinTaekLim <jintaek019@gmail.com>
2024-11-09 16:03:30 +0900

    [BBB-58] Admin created_at, created_id 추가

--- src/main/java/com/bangbangbwa/backend/domain/admin/common/entity/Admin.java
+++ src/main/java/com/bangbangbwa/backend/domain/admin/common/entity/Admin.java
@@ -1,16 +1,25 @@
 package com.bangbangbwa.backend.domain.admin.common.entity;
 
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import org.apache.ibatis.type.Alias;
 
+@Alias("admin")
 @Getter
 @AllArgsConstructor
-@NoArgsConstructor
 public class Admin {
 
+  private final String SYSTEM = "SYSTEM";
+
   private Long id;
+  private String createdId;
+  private LocalDateTime createdAt;
 
-
-
+  @Builder
+  public Admin() {
+    this.createdId = SYSTEM;
+    this.createdAt = LocalDateTime.now();
+  }
 }
