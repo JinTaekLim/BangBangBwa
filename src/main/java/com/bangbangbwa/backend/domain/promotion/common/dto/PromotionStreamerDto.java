1df0761d2aa04de6034a15f2ec7b350ce2a4a9a0
Zeeeen0 <hoo1042@gmail.com>
2024-11-12 22:01:24 +0900

    [BBB-30] 메인페이지 API 명세서 작성 - Response 이름 수정

--- src/main/java/com/bangbangbwa/backend/domain/promotion/common/dto/PromotionStreamerDto.java
+++ src/main/java/com/bangbangbwa/backend/domain/promotion/common/dto/PromotionStreamerDto.java
@@ -9,12 +9,13 @@ public class PromotionStreamerDto {
   @Schema(name = "PromotionStreamerResponse", description = "홍보 스트리머 응답")
   public record Response(
       @Schema(description = "스트리머 목록")
-      List<PromotionStreamerResponseStreamer> streamerList
+      List<PromotionStreamer> streamerList
   ) {
 
   }
 
-  public record PromotionStreamerResponseStreamer(
+  @Schema(name = "PromotionStreamerResponseStreamer", description = "홍보 스트리머 응답")
+  public record PromotionStreamer(
       @Schema(description = "스트리머 오늘의 한마디", example = "한마디")
       String todayComment,
       @Schema(description = "스트리머 자기소개", example = "안녕하세요 이번에 새로 시작하게된 스트리머 이수연 입니다.")
@@ -22,16 +23,17 @@ public class PromotionStreamerDto {
       @Schema(description = "스트리머 사진 URL", example = "이미지 URL")
       String imageUrl,
       @Schema(description = "스트리머 관심분야 목록")
-      List<PromotionStreamerResponseStreamerInterested> interestedList,
+      List<PromotionStreamerInterested> interestedList,
       @Schema(description = "스트리머 플랫폼 목록")
-      List<PromotionStreamerResponseStreamerPlatform> platformList,
+      List<PromotionStreamerPlatform> platformList,
       @Schema(description = "스트리머 이름", example = "이수연")
       String name
   ) {
 
   }
 
-  public record PromotionStreamerResponseStreamerInterested(
+  @Schema(name = "PromotionStreamerResponseStreamerInterested", description = "홍보 스트리머 응답 - 스트리머 관심분야 목록")
+  public record PromotionStreamerInterested(
       @Schema(description = "관심 여부", example = "true")
       boolean isInterested,
       @Schema(description = "관심분야 이름", example = "음악")
@@ -40,7 +42,8 @@ public class PromotionStreamerDto {
 
   }
 
-  public record PromotionStreamerResponseStreamerPlatform(
+  @Schema(name = "PromotionStreamerResponseStreamerPlatform", description = "홍보 스트리머 응답 - 스트리머 플랫폼 목록")
+  public record PromotionStreamerPlatform(
       @Schema(description = "플랫폼 종류", allowableValues = {"SOOP", "YOUTUBE", "CHZZK"})
       Platform platform,
       @Schema(description = "플랫폼 로고 사진 URL", example = "플랫폼 로고 사진 URL")
