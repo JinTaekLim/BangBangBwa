5d9b876ba9d091db47838996ac4840b344290720
Zeeeen0 <hoo1042@gmail.com>
2024-11-20 22:46:21 +0900

    [BBB-75] MyBatis 매핑에 필요한 NoArgs 추가

--- src/main/java/com/bangbangbwa/backend/domain/member/common/dto/ProfileDto.java
+++ src/main/java/com/bangbangbwa/backend/domain/member/common/dto/ProfileDto.java
@@ -5,7 +5,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class ProfileDto {
