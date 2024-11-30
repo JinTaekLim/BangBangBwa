60c28833ebf0d3c93d1c8f25a3342f0c63af156c
limjintaek <jintaek019@gmail.com>
2024-11-30 16:26:58 +0900

    [BBB-65] 방송 계정 승인 대기 중 사용자 재신청 불가능하도록 변경

--- src/main/java/com/bangbangbwa/backend/domain/streamer/common/business/PendingStreamerGenerator.java
+++ src/main/java/com/bangbangbwa/backend/domain/streamer/common/business/PendingStreamerGenerator.java
@@ -3,7 +3,6 @@ package com.bangbangbwa.backend.domain.streamer.common.business;
 import com.bangbangbwa.backend.domain.admin.common.dto.ApproveStreamerDto;
 import com.bangbangbwa.backend.domain.admin.common.entity.Admin;
 import com.bangbangbwa.backend.domain.member.common.dto.PromoteStreamerDto;
-import com.bangbangbwa.backend.domain.member.common.entity.Member;
 import com.bangbangbwa.backend.domain.streamer.common.entity.PendingStreamer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -14,9 +13,9 @@ public class PendingStreamerGenerator {
 
   private final PendingStreamerParser pendingStreamerParser;
 
-  public PendingStreamer generate(PromoteStreamerDto.Request request, Member member) {
+  public PendingStreamer generate(PromoteStreamerDto.Request request, Long memberId) {
     PendingStreamer pendingStreamer = pendingStreamerParser.requestToEntity(request);
-    pendingStreamer.updateMemberId(member.getId());
+    pendingStreamer.updateMemberId(memberId);
     return pendingStreamer;
   }
 
