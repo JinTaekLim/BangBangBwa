46dc40f0cc2bdaa096f64b46510b34f71fea6ade
Zeeeen0 <hoo1042@gmail.com>
2024-12-04 11:33:54 +0900

    [BBB-75] RandomString 팩토리 메소드 패턴으로 변경

--- src/test/java/com/bangbangbwa/backend/global/util/randomValue/RandomString.java
+++ src/test/java/com/bangbangbwa/backend/global/util/randomValue/RandomString.java
@@ -2,16 +2,11 @@ package com.bangbangbwa.backend.global.util.randomValue;
 
 public class RandomString {
 
-  private static RandomStringGenerator generator;
-
   private RandomString() {
     throw new AssertionError("Utility class should not be instantiated");
   }
 
   public static RandomStringGenerator getGenerator() {
-    if (generator == null) {
-      generator = new RandomStringGenerator();
-    }
-    return generator;
+    return new RandomStringGenerator();
   }
 }
