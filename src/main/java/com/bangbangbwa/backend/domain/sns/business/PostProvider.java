f99e9226d0dac6f7cb0afa8836a263bd5d4ee2c3
limjintaek <jintaek019@gmail.com>
2024-12-15 16:37:16 +0900

    [BBB-98] 게시물 추천 로직 변경

--- src/main/java/com/bangbangbwa/backend/domain/sns/business/PostProvider.java
+++ src/main/java/com/bangbangbwa/backend/domain/sns/business/PostProvider.java
@@ -3,7 +3,6 @@ package com.bangbangbwa.backend.domain.sns.business;
 import com.bangbangbwa.backend.domain.sns.common.entity.Post;
 import com.bangbangbwa.backend.domain.sns.common.enums.PostType;
 import java.util.*;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -11,14 +10,8 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class PostProvider {
 
-  // 게시물 반환 최대 갯수
+  // 게시물 반환 갯수
   private final int POST_SIZE = 7;
-  // 팔로우 게시물 반환 갯수
-  private int FOLLOW_POST_SIZE;
-  // 태그 게시물 반환 갯수
-  private int TAG_POST_SIZE;
-  // 현재까지 조회된 게시물
-  private Set<String> excludedPostIds = new HashSet<>();
 
 
   private final PostRecommendationStrategy postRecommendationStrategy;
@@ -32,58 +25,35 @@ public class PostProvider {
   }
 
   public List<Post> getMemberPersonalizedPosts(Long memberId, Set<String> readPostIds) {
-    initializePostSetup(readPostIds);
-    List<Post> followPost = getFollowPosts(memberId);
-    List<Post> tagPost = getTagPost(memberId);
-    List<Post> randomPost = getRandomPostsForMissingCount(PostType.STREAMER);
-    return mergeUniquePosts(followPost, tagPost, randomPost);
-  }
-
-  private void initializePostSetup(Set<String> readPostIds) {
-    setPostSize();
-    setExcludedPostIds(readPostIds);
-  }
-
-  private void setPostSize() {
-    int[] postProbability = postRecommendationStrategy.getPostReturnCount(POST_SIZE);
-    FOLLOW_POST_SIZE = postProbability[0];
-    TAG_POST_SIZE = postProbability[1];
-  }
-
-  private void setExcludedPostIds(Set<String> readPostIds) {
-    excludedPostIds.addAll(readPostIds);
+    List<Post> followPost = getFollowPosts(memberId, readPostIds);
+    List<Post> tagPost = getTagPost(memberId, readPostIds);
+    List<Post> randomPost = getRandomPost(PostType.STREAMER, readPostIds);
+    return postRecommendationStrategy.getPosts(followPost, tagPost, randomPost);
   }
 
 
-  private List<Post> getFollowPosts(Long memberId) {
-    List<Post> postList = postReader.findPostsByFollowStreamerExcludingReadIds(FOLLOW_POST_SIZE, memberId, excludedPostIds);
-    addExcludedPostIds(postList);
-    return postList;
+  private List<Post> getFollowPosts(Long memberId, Set<String> readPostIds) {
+    List<Post> posts = postReader.findPostsByFollowStreamerExcludingReadIds(POST_SIZE, memberId, readPostIds);
+    addReadPostIds(posts, readPostIds);
+    return posts;
   }
 
-  public List<Post> getTagPost(Long memberId) {
-    List<Post> postList = postReader.findPostsByFollowedStreamerExcludingReadIds(TAG_POST_SIZE, memberId, excludedPostIds);
-    addExcludedPostIds(postList);
-    return postList;
+  private List<Post> getTagPost(Long memberId, Set<String> readPostIds) {
+    List<Post> posts =  postReader.findPostsByFollowedStreamerExcludingReadIds(POST_SIZE, memberId, readPostIds);
+    addReadPostIds(posts, readPostIds);
+    return posts;
   }
 
-  private List<Post> getRandomPostsForMissingCount(PostType postType) {
-    int postSize = POST_SIZE - excludedPostIds.size();
-    return postReader.findRandomPostsExcludingReadIds(postType,postSize, excludedPostIds);
+  private List<Post> getRandomPost(PostType postType, Set<String> readPostIds) {
+    List<Post> posts =  postReader.findRandomPostsExcludingReadIds(postType,POST_SIZE, readPostIds);
+    addReadPostIds(posts, readPostIds);
+    return posts;
   }
 
-  public void addExcludedPostIds(List<Post> postList) {
-    excludedPostIds.addAll(postList.stream()
-            .map(post -> Long.toString(post.getId()))
-            .collect(Collectors.toSet()));
+  private void addReadPostIds(List<Post> posts, Set<String> readPostIds) {
+    posts.stream()
+        .map(Post::getId)
+        .map(String::valueOf)
+        .forEach(readPostIds::add);
   }
-
-  private List<Post> mergeUniquePosts(List<Post> followPost, List<Post> tagPost, List<Post> randomPost) {
-    Set<Post> uniquePosts = new HashSet<>();
-    uniquePosts.addAll(followPost);
-    uniquePosts.addAll(tagPost);
-    uniquePosts.addAll(randomPost);
-    return new ArrayList<>(uniquePosts);
-  }
-
 }
--- src/main/java/com/bangbangbwa/backend/domain/sns/business/PostRecommendationStrategy.java
+++ src/main/java/com/bangbangbwa/backend/domain/sns/business/PostRecommendationStrategy.java
@@ -1,40 +1,70 @@
 package com.bangbangbwa.backend.domain.sns.business;
 
+import com.bangbangbwa.backend.domain.sns.common.entity.Post;
+import java.util.stream.Stream;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
 
 @Component
 public class PostRecommendationStrategy {
 
-    private final ThreadLocalRandom rand = ThreadLocalRandom.current();
+    private static final ThreadLocalRandom RAND = ThreadLocalRandom.current();
 
     // 각 게시물에 대한 확률 (팔로우한 사람, 비슷한 태그, 랜덤)
-    private static final int[][] probabilities = {
-            {70, 20, 10}, // 첫 번째 게시물
-            {60, 25, 15}, // 두 번째 게시물
-            {50, 30, 20}, // 이후    게시물
+    private static final int[][] PROBABILITIES = {
+        {70, 20, 10}, // 첫 번째 게시물
+        {60, 25, 15}, // 두 번째 게시물
+        {50, 30, 20}  // 이후 게시물
     };
 
+    private static final int MAX_POSTS = 7;
 
-    public int[] getPostReturnCount(int postSize) {
-        int[] result = {0, 0, 0}; // [0: 팔로우한 사람, 1: 비슷한 태그, 2: 랜덤]
+    public List<Post> getPosts(
+        List<Post> followPosts, List<Post> tagPosts, List<Post> randomPosts
+    ) {
+        List<Post> selectedPosts = new ArrayList<>();
+        IntStream.range(0, MAX_POSTS)
+            .mapToObj(i -> selectPost(i, followPosts, tagPosts, randomPosts, selectedPosts))
+            .filter(Objects::nonNull)
+            .forEach(selectedPosts::add);
+        return selectedPosts;
+    }
 
-        for (int i = 0; i < postSize; i++) {
-            int randomValue = rand.nextInt(100);
-            int[] currentProbabilities = probabilities[Math.min(i, probabilities.length - 1)];
 
-            int followProbability = currentProbabilities[0];
-            int tagProbability = followProbability + currentProbabilities[1];
+    private Post selectPost(
+        int num,
+        List<Post> followPosts,
+        List<Post> tagPosts,
+        List<Post> randomPosts,
+        List<Post> selectedPosts
+    ) {
+        int randomValue = RAND.nextInt(100);
+        int[] currentProbabilities = PROBABILITIES[Math.min(num, PROBABILITIES.length - 1)];
 
-            if (randomValue < followProbability) {
-                result[0]++;
-            } else if (randomValue < tagProbability) {
-                result[1]++;
-            } else {
-                result[2]++;
-            }
+        if (randomValue < currentProbabilities[0]) {
+            return findUniquePost(followPosts, tagPosts, randomPosts, selectedPosts);
+        } else if (randomValue < currentProbabilities[0] + currentProbabilities[1]) {
+            return findUniquePost(tagPosts, followPosts, randomPosts, selectedPosts);
+        } else {
+            return findUniquePost(randomPosts, followPosts, tagPosts, selectedPosts);
         }
-        return result;
+    }
+
+    private Post findUniquePost(
+        List<Post> firstPosts,
+        List<Post> secondPosts,
+        List<Post> thirdPosts,
+        List<Post> selectedPosts
+    ) {
+        return Stream.of(firstPosts, secondPosts, thirdPosts)
+            .flatMap(List::stream)
+            .filter(post -> !selectedPosts.contains(post))
+            .findFirst()
+            .orElse(null);
     }
 }
