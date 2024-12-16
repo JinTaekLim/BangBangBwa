579503ed00bd5bc155af6fc41f2d1bbf9ea6b9c4
hyensukim <crazymin17@naver.com>
2024-12-16 12:21:51 +0900

    [BBB-47] CORS 중복 설정 제거

--- src/main/java/com/bangbangbwa/backend/global/config/SecurityConfig.java
+++ src/main/java/com/bangbangbwa/backend/global/config/SecurityConfig.java
@@ -1,8 +1,6 @@
 package com.bangbangbwa.backend.global.config;
 
 import com.bangbangbwa.backend.global.config.log.LogFilter;
-import java.util.Arrays;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,9 +12,6 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.context.SecurityContextHolderFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @RequiredArgsConstructor
@@ -48,23 +43,5 @@ public class SecurityConfig {
     ;
 
     return http.build();
-  }
-
-  @Bean
-  public CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(Arrays.asList(
-        "http://localhost:3000",
-        "https://dev-bangbangbwa.netlify.app"
-    ));
-    configuration.setAllowedMethods(Arrays.asList(
-        "GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"
-    ));
-    configuration.setAllowedHeaders(List.of("*"));
-    configuration.setAllowCredentials(true);
-
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", configuration);
-    return source;
   }
 }
