ac4486a39bdf12416363b39233bcda7d8fa8b587
limjintaek <jintaek019@gmail.com>
2024-12-17 15:28:16 +0900

    [BBB-113] 신고된 게시물 조회

---
+++ src/main/java/com/bangbangbwa/backend/domain/admin/common/dto/GetReportedPostsDto.java
@@ -0,0 +1,30 @@
+package com.bangbangbwa.backend.domain.admin.common.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class GetReportedPostsDto {
+
+  @Schema(name = "GetReportedPostsResponse", description = "신고된 게시물 조회 응답")
+  public record Response(
+      List<GetReportedPostsResponse> posts
+  ){ }
+
+  @Schema(name = "GetReportedPostsResponses")
+  public record GetReportedPostsResponse(
+      @Schema(description = "게시물 ID")
+      Long postId,
+      @Schema(description = "작성자 ID")
+      Long writerId,
+      @Schema(description = "작성자 닉네임")
+      String nickname,
+      @Schema(description = "작성자 프로필 URL")
+      String profile,
+      @Schema(description = "신고 사유")
+      String reason,
+      @Schema(description = "신고 날짜")
+      LocalDateTime reportDate
+  ) { }
+
+}
--- src/main/java/com/bangbangbwa/backend/domain/sns/common/dto/ReportPostDto.java
+++ src/main/java/com/bangbangbwa/backend/domain/sns/common/dto/ReportPostDto.java
@@ -9,6 +9,9 @@ public class ReportPostDto {
     public record Request(
             @Schema(description = "게시글 ID", examples = "1,2")
             @NotNull(message = "게시물 ID를 입력해주세요.")
-            Long postId
+            Long postId,
+            @Schema(description = "신고 사유", example = "불법 광고")
+            @NotNull(message = "신고 사유를 일벽해주세요.")
+            String reason
     ){}
 }
