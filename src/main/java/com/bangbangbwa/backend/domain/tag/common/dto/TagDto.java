d82d7153aa4431ffcad5796955135fcf946c602b
hyensukim <crazymin17@naver.com>
2024-12-19 00:05:54 +0900

    [BBB-122] TagDto 생성자 수정

--- src/main/java/com/bangbangbwa/backend/domain/tag/common/dto/TagDto.java
+++ src/main/java/com/bangbangbwa/backend/domain/tag/common/dto/TagDto.java
@@ -3,10 +3,8 @@ package com.bangbangbwa.backend.domain.tag.common.dto;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
 public class TagDto {
