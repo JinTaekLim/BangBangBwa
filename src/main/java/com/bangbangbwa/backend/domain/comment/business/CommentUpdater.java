9a61b346305278837307ebddfd75bd7c6c6b7cf9
hyensukim <crazymin17@naver.com>
2024-12-19 23:59:24 +0900

    [BBB-128] 빌드 실패 관련 오류 임시 조치 - resultMap으로 대체 예정

--- src/main/java/com/bangbangbwa/backend/domain/comment/business/CommentUpdater.java
+++ src/main/java/com/bangbangbwa/backend/domain/comment/business/CommentUpdater.java
@@ -1,5 +1,6 @@
 package com.bangbangbwa.backend.domain.comment.business;
 
+import com.bangbangbwa.backend.domain.member.common.entity.Member;
 import com.bangbangbwa.backend.domain.sns.common.entity.Comment;
 import com.bangbangbwa.backend.domain.sns.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,10 @@ import org.springframework.stereotype.Component;
 public class CommentUpdater {
 
   private final CommentRepository commentRepository;
+
+  public void updateMemberId(Comment comment, Member member) {
+    comment.updateMemberId(member.getId());
+  }
 
   public void updateForAddReply(Comment comment) {
     commentRepository.update(comment);
--- src/main/java/com/bangbangbwa/backend/domain/sns/business/CommentGenerator.java
+++ src/main/java/com/bangbangbwa/backend/domain/sns/business/CommentGenerator.java
@@ -1,5 +1,6 @@
 package com.bangbangbwa.backend.domain.sns.business;
 
+import com.bangbangbwa.backend.domain.comment.business.CommentUpdater;
 import com.bangbangbwa.backend.domain.member.common.entity.Member;
 import com.bangbangbwa.backend.domain.sns.common.dto.CreateCommentDto;
 import com.bangbangbwa.backend.domain.sns.common.entity.Comment;
